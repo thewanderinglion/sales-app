4d4f676e5484cc27df97d75e31a89f367b2f8f15
ny-dude <sj674@nyu.edu>
2019-11-04 14:12:11 +0200

    Customized the admin site with additional fields

--- sales/admin.py
+++ sales/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Customer, Order, Payment
 
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(Payment)
+
+class CustomerAdmin(admin.ModelAdmin):
+    fields = ['first_name', 'last_name', 'email', 'amount_owed']
+    readonly_fields = ['amount_owed']
+
+
+admin.site.register(Customer, CustomerAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    fields = ['customer', 'product', 'product_sale_price', 'date_bought', 'amount_owed', 'payments_total']
+    readonly_fields = ['date_bought', 'amount_owed', 'payments_total']
+
+
+admin.site.register(Order, OrderAdmin)
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    fields = ['order', 'customer', 'payment_type', 'payment_amount', 'date_paid']
+    readonly_fields = ['date_paid']
+
+
+admin.site.register(Payment, PaymentAdmin)
