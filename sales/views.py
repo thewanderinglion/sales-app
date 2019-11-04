fa0ee6a2cb60ee69086ef2b3cc7126b1d6e7ed23
ny-dude <sj674@nyu.edu>
2019-11-04 02:41:26 +0200

    Built 8 views in the views.py file

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Customer, Order, Payment
+from django.contrib import messages
+from .forms import CustomerRegistrationForm, OrderPlacementForm, PaymentAcceptForm
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'sales/home.html')
+
+
+def about(request):
+    return render(request, 'sales/about.html', {'title': 'About Our Sales'})
+
+
+def customer(request):
+    context = {
+        'customers': Customer.objects.all(),
+        'title': 'Customers'
+    }
+    return render(request, 'sales/customer.html', context)
+
+
+def order(request):
+    context = {
+        'orders': Order.objects.all(),
+        'title': 'Orders'
+    }
+    return render(request, 'sales/order.html', context)
+
+
+def payment(request):
+    context = {
+        'payments': Payment.objects.all(),
+        'title': 'Payments'
+    }
+    return render(request, 'sales/payment.html', context)
+
+
+def customer_registration(request):
+    if request.method == 'POST':
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Successfully created new customer!')
+            return redirect('sales-customer')
+    else:
+        form = CustomerRegistrationForm()
+    return render(request, 'sales/customer_registration.html', {'form': form, 'title': 'New Customer Registration'})
+
+
+def order_placement(request):
+    if request.method == 'POST':
+        form = OrderPlacementForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Successfully placed new order!')
+            return redirect('sales-order')
+    else:
+        form = OrderPlacementForm()
+    return render(request, 'sales/order_placement.html', {'form': form, 'title': 'New Order Placement'})
+
+
+def payment_accept(request):
+    if request.method == 'POST':
+        form = PaymentAcceptForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Successfully accepted payment!')
+            return redirect('sales-payment')
+    else:
+        form = PaymentAcceptForm()
+    return render(request, 'sales/payment_accept.html', {'form': form, 'title': 'New Payment'})
